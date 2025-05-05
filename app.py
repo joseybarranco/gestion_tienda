8cd485bf0b3a59b453a60694aa79e7533d21f90b
José Ignacio <joseyenred@hotmail.com>
2025-05-05 11:14:43 +0200

    rutas del app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
@@ -6,6 +8,36 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
+    nombre_admin = "Francisco"
+    tienda = "TecnoMarket"
+    fecha = date.today()
+    productos = [
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        {'nombre': 'raton','precio': 7.55,'stock': 4,'categoria':'electronica'},
+        ]
+    clientes = [
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+        {'nombre': 'Fran','email': 'fran@gmail.com','activo': True,'pedidos': 3},
+    ]
+    pedidos = [
+        {'cliente':'Fran','total':15,'fecha':'12-12-24'}
+    ]
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
